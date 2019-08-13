93b7d07af933f696cbfd87a85e27967e65c9ad9b
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-08-13 13:21:37 +0900

    [FIXED] split data with train and test data

--- data_loader.py
+++ data_loader.py
@@ -9,13 +9,13 @@ import cv2
 def getBagImage():
     print('Reading csv................')
     df = pd.read_csv('./data/training_bag_csv')
-    #data_mask = np.random.rand(len(df)) < 0.8
+    data_mask = np.random.rand(len(df)) < 0.8
     train_df = df
     train_label_df = train_df.drop(['filename'], axis=1)
-    #test_df = df[~data_mask]
-    #test_label_df = test_df.drop(['filename'], axis=1)
+    test_df = df[~data_mask]
+    test_label_df = test_df.drop(['filename'], axis=1)
     train_data = []
-    #test_data = []
+    test_data = []
     print('Load bag image...................')
     for i in range(len(train_df)):
         train_image = cv2.imread(train_df['filename'][i])
@@ -24,16 +24,18 @@ def getBagImage():
         T[:,:,:] = train_image
         train_label = train_label_df.values.tolist()
         train_data.append((T, train_label))
-#     for i in range(len(test_df)):
-#         test_image = cv2.imread(test_df['filename'][i])
-#         test_label = test_label_df.values.tolist()
-#         test_data.append(test_image)
-#         test_data.append(test_label)
+    for i in range(len(test_df)):
+        test_image = cv2.imread(test_df['filename'[i]]
+        test_image = cv2.resize(test_image, (64,64))
+        S = np.zeros([64, 64, 3])
+        S[:,:,:] = test_image
+        test_label = test_label_df.values.tolist()
+        test_data.append((S, test_label))
 
     print('Number of bag train data: ', str(len(train_data)))
-    #print('Number of bag test data: ', str(len(test_data)))
+    print('Number of bag test data: ', str(len(test_data)))
     print('---------------------------------------------------------------')
-    return train_data
+    return train_data, test_data
 
 #########################################################################################
 #
